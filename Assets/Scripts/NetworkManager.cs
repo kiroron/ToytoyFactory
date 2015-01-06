1cb432d7697a894911bc2d63a130432969e82abe
maitonn <maitonn428@gmail.com>
2015-01-06 09:31:39 +0900

    ファーストコミット！

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+    string registeredGameName = "Test";
+    private void StartServer()
+    {
+        Network.InitializeServer(32, 25000, false);
+        MasterServer.RegisterHost(registeredGameName,"Networking Turoial Game");
+    }
+}
